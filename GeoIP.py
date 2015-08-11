0cee388883ce4e9205b1161b1f5154bd61cd3bd1
Nemanja Djurcic <no1knows@cyberdude.com>
2015-08-11 03:23:26 +0200

    Update GeoIP.py

--- GeoIP.py
+++ GeoIP.py
@@ -6,12 +6,12 @@ from lxml import html
 
 r = requests.get(r'http://jsonip.com')
 ip = r.json()['ip']
+print ip
 resp = requests.get("http://whatismyipaddress.com/ip/" + ip)
 doc = html.fromstring(resp.text)
 country_xp = '//*[@id="section_left_3rd"]/table/tr[1]/td[1]/text()[1]'
 region_xp = '//*[@id="section_left_3rd"]/table/tr[2]/td[1]/text()[1]'
 city_xp = '//*[@id="section_left_3rd"]/table/tr[3]/td[1]/text()[1]'
-print ip
 print doc.xpath(country_xp)[0]
 print doc.xpath(region_xp)[0]
 print doc.xpath(city_xp)[0]
